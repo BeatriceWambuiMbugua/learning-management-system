8974394d2d527f7f9904ef544481738780aa3e3c
BeatriceWambuiMbugua <wambuimbeatrice@gmail.com>
2024-09-01 07:11:04 +0300

    Add POST function to handle course creation - For handling course creation requests and error logging.

---
+++ app/api/courses/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server"
+
+export async function POST (req:Request){
+    try {
+        
+    } catch (error) {
+        console.log("[Courses]", error)
+        return new NextResponse("Internal Error", { status: 500 })
+    }
+
+}

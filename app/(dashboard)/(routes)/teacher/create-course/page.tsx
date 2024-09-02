3fb0f001f2bd7e51f754f95a5ebb20bbc3ab3473
BeatriceWambuiMbugua <wambuimbeatrice@gmail.com>
2024-09-02 15:19:08 +0300

    Add success and error toast notifications for course creation feedback - Enhancing user experience with informative toast messages.

--- app/(dashboard)/(routes)/teacher/create-course/page.tsx
+++ app/(dashboard)/(routes)/teacher/create-course/page.tsx
@@ -32,6 +32,7 @@ const CreateCourse = () => {
         try {
             const response = await axios.post("/api/courses", values)
             router.push(`/teacher/courses/${response.data.id}`)
+            toast.success("Course created")
         } catch {
             toast.error("Something went wrong")
         }

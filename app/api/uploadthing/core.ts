239a01e78bf2fba56fb5a1f8df064823dca521b2
BeatriceWambuiMbugua <wambuimbeatrice@gmail.com>
2024-09-04 14:24:56 +0300

    Add error handling to handleAuth function in core.ts - Enhancing API security by ensuring proper authorization checks.

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -9,8 +9,6 @@ const handleAuth = () => {
   if (!userId) throw new Error("Unauthorized")
   return { userId }
 }
-
-
 export const ourFileRouter = {
 
   courseImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } }).middleware(() => handleAuth()).onUploadComplete(() => { }),

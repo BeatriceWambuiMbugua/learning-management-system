4eba3285aa620bbabc775b21a75ab4b9ebf60d4e
BeatriceWambuiMbugua <wambuimbeatrice@gmail.com>
2024-09-09 10:56:07 +0300

    Update navbar component to include user authentication status; Enhances user experience.

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -4,6 +4,7 @@ import { MobileNavbar } from "./mobile-navbar"
 export const Navbar = () =>{
     return (
         <div className="p-4 border-b  h-full flex items-center bg-white shadow-sm">
+            Testing
             <MobileNavbar/>
             <NavbarRoutes/>
         </div>
